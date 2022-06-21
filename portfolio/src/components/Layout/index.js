a14b10f722e37381df73ea419c4b664162e5c827
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-06-21 19:05:53 +0300

    building layout

--- portfolio/src/components/Layout/index.js
+++ portfolio/src/components/Layout/index.js
@@ -1,11 +1,21 @@
 import './index.scss';
 import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <>
+        <div className="App">
             <Sidebar />
-        </>
+            <div className='page'>
+                <span className='tags top-tags'></span>
+                <Outlet />
+                <span className='tags bottom-tags'>
+                &lt;/body&gt;
+                <br/>
+                <span className='bottom-tag-html'>&lt;/html&gt;</span>   
+                </span>
+            </div>
+        </div>
     )
 }
 export default Layout
