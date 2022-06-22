243572ba4a3fa04e26df3a8a89337248a5da473b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-06-22 17:57:48 +0300

    About page

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,16 @@
+import './index.sass'
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    About Me
+                </h1>
+                <p>Hi I'm student at Karabuk University. My Department is Computer Engineering</p>
+                <p>Hi I'm student at Karabuk University. My Department is Computer Engineering</p>
+                <p>Hi I'm student at Karabuk University. My Department is Computer Engineering</p>
+            </div>
+        </div>
+    )
+}
+export default About
